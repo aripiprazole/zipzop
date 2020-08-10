c1d457fe9fa77c7f3c2af79531d677171c633e53
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-10 01:58:09 -0300

    Initial commit

---
+++ backend/build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+  application
+  kotlin("jvm") version "1.3.70"
+}
+
+group = "com.lorenzoog"
+version = "0.0.1"
+
+application {
+  mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+  mavenLocal()
+  jcenter()
+  maven { url = uri("https://kotlin.bintray.com/ktor") }
+  maven { url = uri("https://kotlin.bintray.com/kotlinx") }
+}
+
+dependencies {
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+  implementation("io.ktor:ktor-server-netty:$ktor_version")
+  implementation("ch.qos.logback:logback-classic:$logback_version")
+  implementation("io.ktor:ktor-server-core:$ktor_version")
+  implementation("io.ktor:ktor-locations:$ktor_version")
+  implementation("io.ktor:ktor-server-host-common:$ktor_version")
+  implementation("io.ktor:ktor-auth:$ktor_version")
+  implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+  implementation("io.ktor:ktor-client-core:$ktor_version")
+  implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+//  implementation("io.ktor:ktor-client-http-timeout:$ktor_version")
+  implementation("io.ktor:ktor-client-auth-jvm:$ktor_version")
+  implementation("io.ktor:ktor-client-json-jvm:$ktor_version")
+  implementation("io.ktor:ktor-client-gson:$ktor_version")
+  implementation("io.ktor:ktor-client-cio:$ktor_version")
+  implementation("io.ktor:ktor-websockets:$ktor_version")
+  implementation("io.ktor:ktor-client-websockets:$ktor_version")
+  implementation("io.ktor:ktor-client-logging-jvm:$ktor_version")
+  testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+}
+
+tasks.withType<KotlinCompile>() {
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+    jvmTarget = "1.8"
+  }
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src")
+kotlin.sourceSets["test"].kotlin.srcDirs("test")
+
+sourceSets["main"].resources.srcDirs("resources")
+sourceSets["test"].resources.srcDirs("testresources")
