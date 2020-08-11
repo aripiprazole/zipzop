4e8a2a2ddda759f2d535ee59c7b4f459973fbe4d
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-11 07:17:27 -0300

    feat(backend): add jwt auth

--- backend/src/config/auth/config.kt
+++ backend/src/config/auth/config.kt
@@ -1,19 +1,62 @@
 package com.lorenzoog.zipzop.config.auth
 
-import com.lorenzoog.zipzop.entities.User
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.auth.Authentication.Configuration
-import io.ktor.auth.basic
-import io.ktor.sessions.get
-import io.ktor.sessions.sessions
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.auth.jwt.jwt
+import io.ktor.config.ApplicationConfig
+import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.kodein.di.DI
+import org.kodein.di.instance
+import java.nio.file.Files
+import java.nio.file.Path
+import java.security.KeyFactory
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 
-data class UserPrincipal(val user: User)
+@OptIn(KtorExperimentalAPI::class)
+fun Configuration.setup(di: DI, config: ApplicationConfig) {
+  val jwtIssuer = config.property("domain").getString()
+  val jwtAudience = config.property("audience").getString()
 
-fun Configuration.setup() {
-  basic("jwt") {
-    validate { // TODO
-      null
+  jwt {
+    realm = config.property("realm").getString()
+
+    val algorithm by di.instance<Algorithm>()
+
+    verifier(
+      JWT.require(algorithm)
+        .withAudience(jwtAudience)
+        .withIssuer(jwtIssuer)
+        .build()
+    )
+
+    validate { credentials ->
+      if (credentials.payload.audience.contains(jwtAudience))
+        JWTPrincipal(credentials.payload)
+      else null
     }
-
-    skipWhen { call -> call.sessions.get<UserPrincipal>() != null }
   }
+}
+
+@Suppress("UNCHECKED_CAST")
+suspend fun <PrivateKeyType, PublicKeyType> KeyFactory.getKeyPair(
+  publicKeyPath: String,
+  privateKeyPath: String
+): Pair<PublicKeyType, PrivateKeyType> {
+  val publicKey = withContext(Dispatchers.IO) {
+    val bytes: ByteArray = Files.readAllBytes(Path.of(publicKeyPath))
+
+    generatePublic(X509EncodedKeySpec(bytes)) as PublicKeyType
+  }
+  val privateKey = withContext(Dispatchers.IO) {
+    val bytes: ByteArray = Files.readAllBytes(Path.of(privateKeyPath))
+
+    generatePrivate(PKCS8EncodedKeySpec(bytes)) as PrivateKeyType
+  }
+
+  return publicKey to privateKey
 }
--- backend/src/config/kodein/config.kt
+++ backend/src/config/kodein/config.kt
@@ -2,6 +2,8 @@ package com.lorenzoog.zipzop.config.kodein
 
 import com.lorenzoog.zipzop.services.user.ExposedUserService
 import com.lorenzoog.zipzop.services.user.UserService
+import io.ktor.config.ApplicationConfig
+import io.ktor.util.KtorExperimentalAPI
 import org.kodein.di.DI
 import org.kodein.di.DI.MainBuilder
 import org.kodein.di.bind
@@ -9,8 +11,10 @@ import org.kodein.di.singleton
 
 const val MAIN_MODULE_NAME = "Main module"
 
-fun MainBuilder.setup() {
+@OptIn(KtorExperimentalAPI::class)
+fun MainBuilder.setup(config: ApplicationConfig) {
   import(DI.Module(MAIN_MODULE_NAME) {
     bind<UserService>() with singleton { ExposedUserService() }
   })
+  import(jwtModule(config.config("ktor.jwt")))
 }
