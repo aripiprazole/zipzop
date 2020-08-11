efddd02872ec344298aaf95f8c1d3eadda787146
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-11 03:04:17 -0300

    build(backend): add version to h2 database driver

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
   implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
   implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
 
-  compileOnly("com.h2database:h2:1.4.200") // TODO: remove this, just for local environment
+  runtimeOnly("com.h2database:h2:1.4.200") // TODO: remove this, just for local environment
 
   testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
