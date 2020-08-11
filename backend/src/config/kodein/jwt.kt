208886adc2556379f3c37c0b26d280ad61792bdf
Lorenzo Guimaraes de Oliveira <lorenzooguimaraes@gmail.com>
2020-08-11 08:24:10 -0300

    fix(backend): use hmac256 temporarily, to avoid errors with private and public keys

--- backend/src/config/kodein/jwt.kt
+++ backend/src/config/kodein/jwt.kt
@@ -1,30 +1,27 @@
 package com.lorenzoog.zipzop.config.kodein
 
 import com.auth0.jwt.algorithms.Algorithm
-import com.lorenzoog.zipzop.config.auth.getKeyPair
 import io.ktor.config.ApplicationConfig
 import io.ktor.util.KtorExperimentalAPI
-import kotlinx.coroutines.runBlocking
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.singleton
-import java.security.KeyFactory
-import java.security.interfaces.RSAPrivateKey
-import java.security.interfaces.RSAPublicKey
 
 @OptIn(KtorExperimentalAPI::class)
 fun jwtModule(config: ApplicationConfig) =
   DI.Module("Jwt module") {
     bind<Algorithm>() with singleton {
-      runBlocking<Algorithm> {
-        val rsaPrivateKeyPath = config.property("key.private").getString()
-        val rsaPublicKeyPath = config.property("key.public").getString()
-
-        val keyFactory: KeyFactory = KeyFactory.getInstance("RSA")
-        val (publicKey, privateKey) = keyFactory
-          .getKeyPair<RSAPublicKey, RSAPrivateKey>(rsaPrivateKeyPath, rsaPublicKeyPath)
-
-        Algorithm.RSA512(privateKey, publicKey)
-      }
+      Algorithm.HMAC256("temp secret")
+//      TODO: use RSA512 instead of HMAC256
+//      runBlocking<Algorithm> {
+//        val rsaPrivateKeyPath = config.property("key.private").getString()
+//        val rsaPublicKeyPath = config.property("key.public").getString()
+//
+//        val keyFactory: KeyFactory = KeyFactory.getInstance("RSA")
+//        val (publicKey, privateKey) = keyFactory
+//          .getKeyPair<RSAPublicKey, RSAPrivateKey>(rsaPrivateKeyPath, rsaPublicKeyPath)
+//
+//        Algorithm.RSA512(privateKey, publicKey)
+//      }
     }
   }
