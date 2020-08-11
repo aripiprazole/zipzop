95a2bd54c69c6397225dc3d2ebb645ff17dade03
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-11 04:54:13 -0300

    feat(backend): add user service

---
+++ backend/src/services/user/UserService.kt
@@ -0,0 +1,19 @@
+package com.lorenzoog.zipzop.services.user
+
+import com.lorenzoog.zipzop.dto.user.UserCreateDTO
+import com.lorenzoog.zipzop.dto.user.UserUpdateDTO
+import com.lorenzoog.zipzop.entities.User
+
+interface UserService {
+ suspend fun create(data: UserCreateDTO): User
+
+  suspend fun findAll(page: Int): Iterable<User>
+
+  suspend fun findByUsername(username: String): User
+  suspend fun findById(id: Long): User
+
+  suspend fun updateById(id: Long, newData: UserUpdateDTO): User
+  suspend fun updateByUsername(username: String, newData: UserUpdateDTO): User
+
+  suspend fun deleteById(id: Long): User
+}
