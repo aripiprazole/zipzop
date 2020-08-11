06c37e8b8dad3bff47f48bc9f59324c6397af971
Lorenzo Guimaraes de Oliveira <lorenzooguimaraes@gmail.com>
2020-08-11 11:19:13 -0300

    feat(backend): add password encoder

---
+++ backend/src/auth/password/Argon2PasswordEncoder.kt
@@ -0,0 +1,11 @@
+package com.lorenzoog.zipzop.config.auth.password
+
+class Argon2PasswordEncoder : PasswordEncoder {
+  override fun matches(password: String, hashedPassword: String): Boolean {
+    TODO("Not yet implemented")
+  }
+
+  override fun encode(password: String): String {
+    TODO("Not yet implemented")
+  }
+}
