43885d9250ebb49e9decfd217680286327115abe
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-11 19:41:58 -0300

    refactor(backend): move some configuration classes to Application Main file

--- backend/src/Application.kt
+++ backend/src/Application.kt
@@ -9,8 +9,11 @@ import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
+import io.ktor.http.cio.websocket.pingPeriod
+import io.ktor.http.cio.websocket.timeout
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Locations
+import io.ktor.request.path
 import io.ktor.routing.Routing
 import io.ktor.serialization.json
 import io.ktor.sessions.Sessions
@@ -20,6 +23,8 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import org.kodein.di.ktor.DIFeature
 import org.kodein.di.ktor.di
+import org.slf4j.event.Level
+import java.time.Duration
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -33,14 +38,27 @@ fun Application.module(testing: Boolean = false) {
   install(ContentNegotiation) {
     json(json = Json(JsonConfiguration.Stable))
   }
-  install(CallLogging) { setup() }
+  install(CallLogging) {
+    level = Level.INFO
 
-  install(DIFeature) { setup(environment.config) }
+    filter { call -> call.request.path().startsWith("/") }
+  }
+  install(DIFeature) {
+    setup(environment.config)
+  }
 
   val di = di()
 
-  install(Authentication) { setup(di, environment.config.config("ktor.jwt")) }
-  install(WebSockets) { setup() }
+  install(Authentication) {
+    setup(di, environment.config.config("ktor.jwt"))
+  }
+
+  install(WebSockets) {
+    pingPeriod = Duration.ofSeconds(15)
+    timeout = Duration.ofSeconds(15)
+    maxFrameSize = Long.MAX_VALUE
+    masking = false
+  }
 
   DatabaseInitializer.setupDatabase(environment.config.config("ktor.database"))
   HttpClientInitializer.setupHttpClient()
