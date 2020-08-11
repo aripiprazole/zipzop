396a5033102bc8077ecc2ab373601abcc9e1fdda
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-11 02:50:17 -0300

    feat(backend): add user entity

--- backend/src/config/database/config.kt
+++ backend/src/config/database/config.kt
@@ -1,19 +1,27 @@
 package com.lorenzoog.zipzop.config.database
 
+import com.lorenzoog.zipzop.tables.Users
 import io.ktor.config.ApplicationConfig
 import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 object DatabaseInitializer {
 
   @KtorExperimentalAPI
-  fun setupDatabase(config: ApplicationConfig) {
+  fun setupDatabase(config: ApplicationConfig) = runBlocking<Unit> {
     val url = config.property("url").getString()
     val driver = config.property("driver").getString()
     val user = config.property("user").getString()
     val password = config.property("password").getString()
 
-    Database.connect(url, driver, user, password)
+    Database.connect(url, driver, user, password).also {
+      newSuspendedTransaction(db = it) {
+        SchemaUtils.create(Users)
+      }
+    }
   }
 
 }
--- backend/src/entities/User.kt
+++ backend/src/entities/User.kt
@@ -1,3 +1,13 @@
 package com.lorenzoog.zipzop.entities
 
-class User
+import com.lorenzoog.zipzop.tables.Users
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+
+class User(id: EntityID<Long>) : LongEntity(id) {
+  var username: String by Users.username
+  var password: String by Users.password
+
+  companion object : LongEntityClass<User>(Users)
+}
