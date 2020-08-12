67ef4be941c9408fab99b89037117822dbefd3f7
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 01:42:27 -0300

    feat(backend): handle any SerializationException and EntityNotFoundException and Authentication/Authorization Exceptions

--- backend/src/config/routing.kt
+++ backend/src/config/routing.kt
@@ -3,7 +3,7 @@ package com.lorenzoog.zipzop.config
 import com.lorenzoog.zipzop.AuthenticationException
 import com.lorenzoog.zipzop.AuthorizationException
 import com.lorenzoog.zipzop.controllers.sessionController
-import com.lorenzoog.zipzop.dto.exceptions.JsonDecodingExceptionDTO
+import com.lorenzoog.zipzop.dto.exceptions.ExceptionDTO
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.StatusPages
@@ -11,7 +11,9 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.response.respond
 import io.ktor.routing.Routing
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.JsonDecodingException
+import org.jetbrains.exposed.dao.exceptions.EntityNotFoundException
 
 @OptIn(KtorExperimentalLocationsAPI::class)
 fun Routing.setupMainRouter() {
@@ -19,15 +21,23 @@ fun Routing.setupMainRouter() {
 
   install(StatusPages) {
     exception<AuthenticationException> {
-      call.respond("ALAMEUPAU NO SEU N AUTENTICADO HAHAHAHHAHAHA")
+      call.respond(HttpStatusCode.Forbidden, ExceptionDTO("Invalid credentials."))
     }
 
     exception<AuthorizationException> {
-      call.respond(HttpStatusCode.Forbidden)
+      call.respond(HttpStatusCode.Forbidden, ExceptionDTO("You are not allowed to access."))
     }
 
     exception<JsonDecodingException> {
-      call.respond(HttpStatusCode.BadRequest, JsonDecodingExceptionDTO("Invalid json!"))
+      call.respond(HttpStatusCode.BadRequest, ExceptionDTO("Invalid json!"))
+    }
+
+    exception<SerializationException> { cause ->
+      call.respond(HttpStatusCode.BadRequest, ExceptionDTO(cause.message.toString()))
+    }
+
+    exception<EntityNotFoundException> {
+      call.respond(HttpStatusCode.NotFound, ExceptionDTO("Not found the requested entity"))
     }
   }
 }
