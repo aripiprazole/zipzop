b5736b10ee9fcdebd9edd43cc565b1e10c640b33
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-12 01:37:23 -0300

    feat(backend): add json decoding exception handler and dto

--- backend/src/config/routing.kt
+++ backend/src/config/routing.kt
@@ -3,6 +3,7 @@ package com.lorenzoog.zipzop.config
 import com.lorenzoog.zipzop.AuthenticationException
 import com.lorenzoog.zipzop.AuthorizationException
 import com.lorenzoog.zipzop.controllers.sessionController
+import com.lorenzoog.zipzop.dto.exceptions.JsonDecodingExceptionDTO
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.StatusPages
@@ -10,18 +11,23 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.response.respond
 import io.ktor.routing.Routing
+import kotlinx.serialization.json.JsonDecodingException
 
 @OptIn(KtorExperimentalLocationsAPI::class)
 fun Routing.setupMainRouter() {
   sessionController()
 
   install(StatusPages) {
-    exception<AuthenticationException> { cause ->
-      call.respond(HttpStatusCode.Unauthorized)
+    exception<AuthenticationException> {
+      call.respond("ALAMEUPAU NO SEU N AUTENTICADO HAHAHAHHAHAHA")
     }
 
-    exception<AuthorizationException> { cause ->
+    exception<AuthorizationException> {
       call.respond(HttpStatusCode.Forbidden)
+    }
+
+    exception<JsonDecodingException> {
+      call.respond(HttpStatusCode.BadRequest, JsonDecodingExceptionDTO("Invalid json!"))
     }
   }
 }
