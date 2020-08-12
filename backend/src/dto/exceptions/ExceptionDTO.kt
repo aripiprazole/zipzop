ca2259d6125e5accb339f5fb8431679f05fc6c13
Lorenzo Guimaraes de Oliveira <lorenzooguimaraes@gmail.com>
2020-08-12 01:41:52 -0300

    refactor(backend): switch to a generic name to use with any exception

--- backend/src/dto/exceptions/ExceptionDTO.kt
+++ backend/src/dto/exceptions/ExceptionDTO.kt
@@ -3,6 +3,6 @@ package com.lorenzoog.zipzop.dto.exceptions
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class JsonDecodingExceptionDTO(
+data class ExceptionDTO(
   val message: String
 )
