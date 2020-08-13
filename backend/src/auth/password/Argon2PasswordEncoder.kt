04c0f4618ac58eeb1ed0dc87f224eb4369313775
Lorenzo Guimaraes de Oliveira <lorenzooguimaraes@gmail.com>
2020-08-13 04:11:46 -0300

    feat(backend): add argon2 password encoder implementation

--- backend/src/auth/password/Argon2PasswordEncoder.kt
+++ backend/src/auth/password/Argon2PasswordEncoder.kt
@@ -1,11 +1,21 @@
 package com.lorenzoog.zipzop.auth.password
 
+import de.mkammerer.argon2.Argon2Factory
+
 class Argon2PasswordEncoder : PasswordEncoder {
+  companion object {
+    const val ITERATIONS = 22
+    const val MEMORY = 64 * 1024
+    const val PARALLELISM = 4
+  }
+
+  private val argon2 = Argon2Factory.create()
+
   override fun matches(password: String, hashedPassword: String): Boolean {
-    TODO("Not yet implemented")
+    return argon2.verify(password, hashedPassword.toByteArray())
   }
 
   override fun encode(password: String): String {
-    TODO("Not yet implemented")
+    return argon2.hash(ITERATIONS, MEMORY, PARALLELISM, password.toByteArray())
   }
 }
