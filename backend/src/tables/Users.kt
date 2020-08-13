650bda4138e37d6d5de8f9ea76b48e7f97ea57cf
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-13 02:01:44 -0300

    fix(backend): make username unique

--- backend/src/tables/Users.kt
+++ backend/src/tables/Users.kt
@@ -3,6 +3,6 @@ package com.lorenzoog.zipzop.tables
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object Users : LongIdTable("users") {
-  val username = varchar("username", 32)
+  val username = varchar("username", 32).uniqueIndex()
   val password = varchar("password", 72) // password hash
 }
