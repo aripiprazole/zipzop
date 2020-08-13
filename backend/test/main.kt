014f905199ec0a7c827bcf23deee48ecad3c9b7c
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-13 01:57:53 -0300

    refactor(backend): refactor tests and use a separated class to SessionController

---
+++ backend/test/main.kt
@@ -0,0 +1,26 @@
+package com.lorenzoog
+
+import com.lorenzoog.zipzop.module
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.HoconApplicationConfig
+import io.ktor.server.testing.TestApplicationEngine
+import io.ktor.server.testing.createTestEnvironment
+import io.ktor.server.testing.withApplication
+import io.ktor.util.KtorExperimentalAPI
+import org.koin.dsl.ModuleDeclaration
+import org.koin.ktor.ext.modules
+
+@OptIn(KtorExperimentalAPI::class)
+fun withMocksApplication(mocks: ModuleDeclaration): (TestApplicationEngine.() -> Unit) -> Unit = { block ->
+  withApplication(createTestEnvironment {
+    config = HoconApplicationConfig(ConfigFactory.load("test-application"))
+
+    module {
+      module(testing = true)
+
+      modules(org.koin.dsl.module(moduleDeclaration = mocks))
+    }
+  }) {
+    block()
+  }
+}
