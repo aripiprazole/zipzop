7b2ef48b19ce7c9d277cc51b89420fef2af7d44c
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-14 01:40:23 -0300

    feat(android): start programming

--- android/app/src/main/java/com/lorenzoog/zipzop/MainActivity.kt
+++ android/app/src/main/java/com/lorenzoog/zipzop/MainActivity.kt
@@ -4,35 +4,31 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Text
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
 import androidx.ui.tooling.preview.Preview
 import com.lorenzoog.zipzop.ui.ZipZopTheme
+import com.lorenzoog.zipzop.ui.components.app.App
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ZipZopTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    setContent {
+      ZipZopTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+          App()
+        }
+      }
+    }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ZipZopTheme {
-        Greeting("Android")
-    }
+  ZipZopTheme {
+    App()
+  }
 }
