738c3581a670c7d8acac4e5b5478175b60c193c5
Lorenzo Guimaraes de Oliveira <lorenzooguimaraes@gmail.com>
2020-08-14 00:21:49 -0300

    fix(backend): make password field bigger to make possible to store passwords' hashes

--- backend/src/tables/Users.kt
+++ backend/src/tables/Users.kt
@@ -4,5 +4,5 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 
 object Users : LongIdTable("users") {
   val username = varchar("username", 32).uniqueIndex()
-  val password = varchar("password", 72) // password hash
+  val password = varchar("password", 128) // password hash
 }
