d7f4ed4a10bffa46a0ee0f0c422439f45826e7a1
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-08-18 10:36:36 -0300

    fix(android): use back press handler

--- android/app/src/main/java/com/lorenzoog/zipzop/MainActivity.kt
+++ android/app/src/main/java/com/lorenzoog/zipzop/MainActivity.kt
@@ -2,27 +2,38 @@ package com.lorenzoog.zipzop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Box
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.platform.setContent
-//import com.lorenzoog.zipzop.database.AppDatabase
-//import com.lorenzoog.zipzop.database.AppDatabaseAmbient
+import com.github.zsoltk.compose.backpress.AmbientBackPressHandler
+import com.github.zsoltk.compose.backpress.BackPressHandler
 import com.lorenzoog.zipzop.ui.App
 import com.lorenzoog.zipzop.ui.ZipZopTheme
 
 class MainActivity : AppCompatActivity() {
+  private val backPressHandler = BackPressHandler()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
       ZipZopTheme {
         // A surface container using the 'background' color from the theme
-        Box(backgroundColor = MaterialTheme.colors.background) {
-//          Providers(AppDatabaseAmbient provides AppDatabase.getDatabase(applicationContext)) {
-          App()
-//          }
+        Surface(color = MaterialTheme.colors.background) {
+          Providers(
+            AmbientBackPressHandler provides backPressHandler
+          ) {
+            App()
+          }
         }
       }
     }
+  }
+
+  override fun onBackPressed() {
+    super.onBackPressed()
+
+    backPressHandler.handle()
   }
 }
